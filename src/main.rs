7af8c8f61cb6407bf140db1061455cff43e5bf79
qwdsx <qwdsx>
2025-04-06 22:05:48 +0300

    remove unused structs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
 use std::env;
-use serde::{Deserialize, Serialize};
 
 use anyhow::Result;
 
 mod tests;
 mod parser;
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Note {
-    __type__: String,
-    fields: Vec<String>,
-    guid: String,
-    note_model_uuid: String,
-    tags: Vec<String>
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Notes {
-    notes: Vec<Note>
-}
 
 fn main() -> Result<()> {
     match env::args().nth(1) {
